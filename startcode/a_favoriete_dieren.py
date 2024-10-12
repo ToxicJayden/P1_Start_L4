a9633271b09f5286e43432b7280f0abde167623e
ToxicJayden <jayden.dingens@gmail.com>
2024-10-12 12:24:57 +0200

    oefening1+2

--- startcode/a_favoriete_dieren.py
+++ startcode/a_favoriete_dieren.py
@@ -3,3 +3,10 @@
 # - drie dieren vraagt aan de user
 # - deze dieren toevoegt aan de lijst
 # - de lijst met dieren toont aan de user
+lijst = []
+print("geef 3 dieren")
+
+for i in range(3):
+    dier = input("dier" +str(i+1)+ ":")
+    lijst.append(dier)
+print(lijst)
--- startcode/b_vind_grootste_getal.py
+++ startcode/b_vind_grootste_getal.py
@@ -1,3 +1,9 @@
 # Maak een lijst met getallen.
 # Schrijf een functie vind_grootste_getal die de grootste waarde uit een lijst teruggeeft.
 # Gebruik een for-loop om de grootste waarde te vinden.
+getal = [1,5,2]
+def vind_grootste_getal_traag():
+    getal.sort()
+    getal.reverse()
+    print(getal[0])
+vind_grootste_getal_traag()
